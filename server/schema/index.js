f74e47545575e6c583d63800a3a8255310bccdba
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-05-23 16:30:19 +0300

    step 3: The Resolve Function & Testing Query

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,5 +1,12 @@
 const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = require("graphql/type");
 
+const movies = [
+  {id: '1', name: 'Pulp Fiction', genre: 'Crime'},
+  {id: '2', name: '1984', genre: 'Sci-Fi'},
+  {id: 3, name: 'V for vendetta', genre: 'Sci-Fi-Triller'},
+  {id: 4, name: 'Snatch', genre: 'Crime-Comedy'},
+];
+
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
@@ -16,7 +23,7 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: {id: {type: GraphQLID}},
       resolve: async (parent, args, context, info) => {
-
+        return movies.find(({id}) => id == args.id);
       }
     },
   })
